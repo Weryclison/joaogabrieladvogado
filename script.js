045abf0a8df84d6e024b33922d66a5034b5767a4
Weryclison Nandi <weryclison.nandi@gmail.com>
2023-11-22 16:11:27 -0300

    add menu mobile

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ body.classList.add("js");
 
 import scrollSuave from "./js/scroll-suave.js";
 import perguntas from "./js/abrir-resposta.js";
+import menuMobile from "./js/menu-mobile.js";
 
 if (body.classList.contains("js")) {
   scrollSuave();
   perguntas();
+  menuMobile();
 }
