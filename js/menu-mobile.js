20fd0a6d23339397d3fa859e1bbf78be8fa7c830
Weryclison Nandi <weryclison.nandi@gmail.com>
2023-11-22 21:01:50 -0300

    add

--- js/menu-mobile.js
+++ js/menu-mobile.js
@@ -1,25 +1,36 @@
-import scrollSuave from "./scroll-suave.js";
 export default function initMenuMobile() {
   document.addEventListener("DOMContentLoaded", function () {
-    const button = document.querySelector('[data-menu="button"]');
-    const menu = document.querySelector("#menu");
-    const linksInterno = document.querySelectorAll(".menu li a");
+    var button = document.querySelector('[data-menu="button"]');
+    var menu = document.querySelector("#menu");
+    var img = document.querySelector('[data-menu="button"] img');
 
-    linksInterno.forEach((item) => {
-      item.addEventListener("click", scrollSuave);
+    menu.addEventListener("click", function (event) {
+      if (event.target.tagName === "A") {
+        button.classList.remove("active");
+        menu.classList.remove("active");
+        img.setAttribute("src", "./img/menu-mobile.svg");
+      }
     });
 
     button.addEventListener("click", function () {
       this.classList.toggle("active");
       menu.classList.toggle("active");
+
+      if (menu.classList.contains("active")) {
+        img.setAttribute("src", "./img/menu-x.svg");
+      } else {
+        img.setAttribute("src", "./img/menu-mobile.svg");
+      }
     });
 
     document.addEventListener("click", function (event) {
-      let isClickInside =
+      var isClickInside =
         menu.contains(event.target) || button.contains(event.target);
       if (!isClickInside) {
         button.classList.remove("active");
         menu.classList.remove("active");
+
+        img.setAttribute("src", "./img/menu-mobile.svg");
       }
     });
   });
